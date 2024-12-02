02b38770ba31b6260cf7002c18091841f36afcc1
Nazadp02 <nazadp02@gmail.com>
2024-12-02 12:11:32 +0100

    ProyectileController && BossAttack
    Nav mesh configuration
    Proyectile movement
    Boss proyectile attack with animations
    Boss movement after attack
    Proyectile destroy

--- Assets/EnemiesScripts/BossAttack.cs
+++ Assets/EnemiesScripts/BossAttack.cs
@@ -1,44 +1,146 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossAttack : MonoBehaviour
 {
 
     #region Components
 
-    //initial properties
-    private Animator Animator;
-
     //shoot properties 
     [Header("Attack Settings")]
-    [SerializeField] private float attackRate = 0.2f;
+    [SerializeField] private float attackRate = 2f;
     [SerializeField] private Transform proyectileShootPoint;
     [SerializeField] private GameObject proyectile;
 
-    private float lastAttackTime;
+    [Header("Movement Settings")]
+    [SerializeField] private int maxShotsBeforeMove = 4;   // Número máximo de disparos antes de moverse
+    [SerializeField] private GameObject[] possiblePositions; // Puntos posibles donde el enemigo puede moverse
 
-    //booleans
+    //private components
+    private float nextShootTime = 0f;
+    private int shootCount = 0;
     private bool isShootingProyectile;
+    private bool canShoot;
+
+    private NavMeshAgent navMeshAgent;
+    private Animator animator;
+
+    private Transform playerTransform;
 
     #endregion
 
+    #region UnityFuntions
+
     private void Start()
     {
-        Animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
-    private bool CanAttack()
+
+    private void Update()
     {
-        if (Time.time - lastAttackTime >= attackRate)
+        ChangeAnimations();
+        AttackAnimation();
+
+        // Si es el momento de disparar, lanza el proyectil
+        if (Time.time >= nextShootTime && navMeshAgent.velocity.sqrMagnitude <= 0.1f)
         {
-            return true; 
+            canShoot = true;
+            ShootProjectile();
+            nextShootTime = Time.time + attackRate;
+        }
+    }
+
+    #endregion
+
+    #region Shoot
+
+    private void ShootProjectile()
+    {
+        if (isShootingProyectile)
+        {
+
+            // Instancia el proyectil y ajusta la dirección hacia el jugador
+            GameObject projectile = Instantiate(proyectile, proyectileShootPoint.position, proyectileShootPoint.rotation);
+
+            //llamamos al script para que gestione el movimiento del proyectil
+            ProyectileController guidedProjectile = projectile.GetComponent<ProyectileController>();
+
+            //incrementamos el número de disparos
+            shootCount++;
+
+            // Si el enemigo ha disparado 4 proyectiles, cambia de posición
+            if (shootCount >= maxShotsBeforeMove)
+            {
+                MoveToNewPosition();
+                canShoot = false;
+                shootCount = 0; // Resetear contador
+            }
+        }
+    }
+
+    #endregion
+
+    #region Movement
+
+    private void MoveToNewPosition()
+    {
+        isShootingProyectile = false;
+
+        // Elige un nuevo punto al azar de los posibles
+        if (possiblePositions.Length > 0)
+        {
+            int randomIndex = Random.Range(0, possiblePositions.Length);
+            Vector3 destination = possiblePositions[randomIndex].transform.position;
+
+            navMeshAgent.SetDestination(destination);
+        }
+    }
+
+    #endregion
+
+    #region Animations
+
+    private void ChangeAnimations()
+    {
+        // Verifica si el enemigo está moviéndose
+        if (navMeshAgent.velocity.sqrMagnitude > 0.1f)
+        {
+            animator.SetBool("IsMoving", true); // Cambiar la animación a "caminar"
+        }
+        else
+        {
+            LookAtPlayer();
+            animator.SetBool("IsMoving", false); // Cambiar la animación a "detenido"
         }
 
-        return false;
     }
 
-    private void ProjectileAttack()
+    private void AttackAnimation()
     {
-        lastAttackTime = Time.time;
-
+        //attack animation
+        if (canShoot == true)
+        {
+            animator.SetBool("attack", true);
+            isShootingProyectile = true;
+        }
+        else
+        {
+            animator.SetBool("attack", false);
+        }
     }
+
+    private void LookAtPlayer()
+    {
+        // Rotar al enemigo para que mire al jugador
+        Vector3 direction = (playerTransform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);  // Rotación suave
+    }
+
+    #endregion
+
 }
+
--- Assets/EnemiesScripts/ProyectileController.cs
+++ Assets/EnemiesScripts/ProyectileController.cs
@@ -1,44 +1,58 @@
 using UnityEngine;
-using System.Collections;
 
 public class ProyectileController : MonoBehaviour
 {
+
+    #region Components
+
     [Header("Proyectile Info")]
-    [SerializeField] private float activeTime = 5;
     [SerializeField] private float proyectileSpeed = 10f;
+    [SerializeField] private float turnSpeed = 5f;
     [SerializeField] private int damage = 5;
+    [SerializeField] private float destroyTime = 5f;
 
     private Rigidbody rb;
+    private Transform playerTransform;
 
     public int Damage { get => damage; set => damage = value; }
 
+    #endregion
+
+    #region UnityFuntions
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        Destroy(gameObject, destroyTime);
     }
 
-
-
-
-
-
-    //When the gameobject SetActive = true
-    private void OnEnable()
+    private void Update()
     {
-        StartCoroutine(DeactiveAfterTime());
+        ProyectileMovement();
     }
 
-    private IEnumerator DeactiveAfterTime()
+    #endregion
+
+    private void ProyectileMovement()
     {
-        yield return new WaitForSeconds(activeTime);
-        gameObject.SetActive(false);
+        // Calcula la dirección hacia el jugador
+        Vector3 direction = (playerTransform.position - transform.position).normalized;
+
+        // Aplica una rotación suave para que el proyectil se oriente hacia el jugador
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, turnSpeed * Time.deltaTime);
+
+        // Mueve el proyectil hacia el jugador
+        rb.velocity = transform.forward * proyectileSpeed;
     }
 
-    //when the bullet collide with something 
     private void OnTriggerEnter(Collider other)
     {
-        //Desactive the bullet
-        gameObject.SetActive(false);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
